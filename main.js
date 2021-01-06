30f1846ffafc7a2cb923aef0f4a147c083a82fb8
Dominic Klaic <KlaicD.github.com>
2021-01-06 20:05:25 +0200

    Add funcFactory and Mutate Method

--- main.js
+++ main.js
@@ -13,10 +13,26 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+const pAequorFactory = (Num, arrayDNA) => {
+  return {
+    specimenNum: Num,
+    dna: arrayDNA,
+    mutate() {
+      let dnaArray = this.dna;
+      // console.log(dnaArray);
+      let randomDNAIndexSelected = Math.floor(Math.random() * this.dna.length); //generates a random number within the lenght of the array (Number)
+      console.log(`Random Base Index Selected: ${randomDNAIndexSelected}`);
+      let selectedBase = this.dna[randomDNAIndexSelected]; // what element is at the Index Number // TEMP!!!!! Letter
+      console.log(`Value at Index: ${selectedBase}`);
+      let replacementBase = returnRandBase(); //generates a new element
+      console.log(`New Base Generated ${replacementBase}`);
+      dnaArray[randomDNAIndexSelected] = replacementBase;
+      return this.dna = dnaArray;
+    }
+  }
+};
 
-
-
-
-
-
-
+const sampleOne = pAequorFactory(1, mockUpStrand());
+console.log(`Original Sample: ${sampleOne.dna}`);
+console.log("Actual Mutation: " + sampleOne.mutate());
+console.log(`Mutated: ${sampleOne.dna}`);
